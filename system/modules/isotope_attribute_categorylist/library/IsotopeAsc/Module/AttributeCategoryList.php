ff9f69570ecd004b4d99ccdecec3e126f90be8ac
asconsulting <andrew@andrewstevens.consulting>
2019-02-08 10:48:12 -0500

    Namespace

--- system/modules/isotope_attribute_categorylist/library/IsotopeAsc/Module/AttributeCategoryList.php
+++ system/modules/isotope_attribute_categorylist/library/IsotopeAsc/Module/AttributeCategoryList.php
@@ -30,7 +30,7 @@ use Isotope\RequestCache\Sort;
 /**
  * Isotope\Module\AttributeCategoryList
  */
-class AttributeCategoryList extends Isotope\Module
+class AttributeCategoryList extends \Isotope\Module
 {
     /**
      * Template
