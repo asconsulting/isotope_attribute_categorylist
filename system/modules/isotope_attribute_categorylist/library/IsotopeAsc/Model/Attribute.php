e5ebc47561236074c0101e010f4fe2bcb09343bb
asconsulting <andrew@andrewstevens.consulting>
2019-02-11 16:23:19 -0500

    Attribute

--- system/modules/isotope_attribute_categorylist/library/IsotopeAsc/Model/Attribute.php
+++ system/modules/isotope_attribute_categorylist/library/IsotopeAsc/Model/Attribute.php
@@ -24,7 +24,7 @@ use Isotope\Translation;
 /**
  * Class IsotopeAsc\Model\Attribute\AttributeCategory
  */
-class Attribute extends Isotope_Attribute;
+class Attribute extends Isotope_Attribute
 {
 
 }
